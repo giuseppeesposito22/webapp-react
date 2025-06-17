57e43558d035f768c54b67369f6e42c1c16d36f4
giuseppeesposito22 <gius.espo22@gmail.com>
2025-06-17 13:30:01 +0200

    add MoviesList component

---
+++ src/components/MoviesList.jsx
@@ -0,0 +1,56 @@
+import Card from "./Card";
+
+const movies = [
+  {
+    id: 1,
+    title: "Inception",
+    director: "Christopher Nolan",
+    genre: "Science Fiction",
+    release_year: 2010,
+    image: "http://localhost:3000/img/movies_cover/inception.jpg",
+  },
+  {
+    id: 2,
+    title: "The Godfather",
+    director: "Francis Ford Coppola",
+    genre: "Crime",
+    release_year: 1972,
+    image: "http://localhost:3000/img/movies_cover/interstellar.jpg",
+  },
+  {
+    id: 3,
+    title: "Titanic",
+    director: "James Cameron",
+    genre: "Romance",
+    release_year: 1997,
+    image: "http://localhost:3000/img/movies_cover/matrix.jpg",
+  },
+  {
+    id: 4,
+    title: "The Matrix",
+    director: "The Wachowskis",
+    genre: "Action",
+    release_year: 1999,
+    image: "http://localhost:3000/img/movies_cover/the_godfather.jpg",
+  },
+  {
+    id: 5,
+    title: "Interstellar",
+    director: "Christopher Nolan",
+    genre: "Science Fiction",
+    release_year: 2014,
+    image: "http://localhost:3000/img/movies_cover/titanic.jpg",
+  },
+];
+
+export default function MoviesList() {
+  return (
+    <div className="container my-5">
+      <div className="row row-cols-3 g-3">
+        {movies.map((movie) => (
+          <Card key={movie.id} movie={movie} />
+        ))}
+      </div>
+    </div>
+  );
+}
