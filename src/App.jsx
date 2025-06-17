c17ea32612e16fa9198598ea1b5f999eaa9fcd7c
giuseppeesposito22 <gius.espo22@gmail.com>
2025-06-17 12:37:15 +0200

    add routers in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import MoviesPage from "./pages/MoviesPage";
+import DefaultLayout from "./layouts/DefaultLayout";
+
 export default function App() {
   return (
     <>
-      <h1>App React</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<DefaultLayout />}>
+            <Route index element={<HomePage />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/movies" element={<MoviesPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
