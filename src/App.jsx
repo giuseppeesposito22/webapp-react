088e5e56b8d0f1b0465cc75bf4b914cc4c88d377
giuseppeesposito22 <gius.espo22@gmail.com>
2025-06-17 16:45:15 +0200

    add route in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import HomePage from "./pages/HomePage";
 import AboutPage from "./pages/AboutPage";
 import MoviesPage from "./pages/MoviesPage";
 import DefaultLayout from "./layouts/DefaultLayout";
+import MoviesDetailPage from "./pages/MoviesDetailPage";
 
 export default function App() {
   return (
@@ -12,7 +13,11 @@ export default function App() {
           <Route element={<DefaultLayout />}>
             <Route index element={<HomePage />} />
             <Route path="/about" element={<AboutPage />} />
-            <Route path="/movies" element={<MoviesPage />} />
+
+            <Route path="/movies">
+              <Route index element={<MoviesPage />} />
+              <Route path=":id" element={<MoviesDetailPage />} />
+            </Route>
           </Route>
         </Routes>
       </BrowserRouter>
