7863e0666c1beeec10b06964eac0f8bd125e4bf1
giuseppeesposito22 <gius.espo22@gmail.com>
2025-06-18 13:34:07 +0200

    add loader in movies list component

--- src/components/MoviesList.jsx
+++ src/components/MoviesList.jsx
@@ -6,23 +6,31 @@ const apiUrl = import.meta.env.VITE_API_URL + "/movies";
 
 export default function MoviesList() {
   const [movies, setMovies] = useState([]);
+  const [isLoad, setIsLoad] = useState(false);
 
   const fetchMovies = () => {
     axios.get(apiUrl).then((res) => {
       setMovies(res.data.data);
       console.log(res.data.data);
+      setIsLoad(true);
     });
   };
 
   useEffect(fetchMovies, []);
 
   return (
-    <div className="container my-5">
-      <div className="row row-cols-3 g-3">
-        {movies.map((movie) => (
-          <MoviesCard key={movie.id} movie={movie} />
-        ))}
-      </div>
-    </div>
+    <>
+      {isLoad ? (
+        <div className="container my-5">
+          <div className="row row-cols-3 g-3">
+            {movies.map((movie) => (
+              <MoviesCard key={movie.id} movie={movie} />
+            ))}
+          </div>
+        </div>
+      ) : (
+        <h1>Loading...</h1>
+      )}
+    </>
   );
 }
