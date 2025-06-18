055368cdfae8133287ecb52f60d9e52e64da0a3b
giuseppeesposito22 <gius.espo22@gmail.com>
2025-06-18 17:00:28 +0200

    add rating star component in review card component

--- src/components/ReviewCard.jsx
+++ src/components/ReviewCard.jsx
@@ -1,9 +1,13 @@
+import RatingStar from "./RatingStar";
+
 export default function ReviewCard({ review }) {
   return (
     <div className="review-card" key={review.id}>
       <div className="user-review">{review.name[0]}</div>
       <div>{review.text}</div>
-      <div>{review.vote}</div>
+      <div>
+        <RatingStar vote={review.vote} />
+      </div>
     </div>
   );
 }
