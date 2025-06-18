b746571a17a4ddb3c6a594c78755afe23c35fc58
giuseppeesposito22 <gius.espo22@gmail.com>
2025-06-18 12:43:59 +0200

    fetch movies in moviesList component

--- src/components/MoviesList.jsx
+++ src/components/MoviesList.jsx
@@ -1,49 +1,21 @@
+import { useEffect, useState } from "react";
 import MoviesCard from "./MoviesCard";
+import axios from "axios";
 
-const movies = [
-  {
-    id: 1,
-    title: "Inception",
-    director: "Christopher Nolan",
-    genre: "Science Fiction",
-    release_year: 2010,
-    image: "http://localhost:3000/img/movies_cover/inception.jpg",
-  },
-  {
-    id: 2,
-    title: "The Godfather",
-    director: "Francis Ford Coppola",
-    genre: "Crime",
-    release_year: 1972,
-    image: "http://localhost:3000/img/movies_cover/the_godfather.jpg",
-  },
-  {
-    id: 3,
-    title: "Titanic",
-    director: "James Cameron",
-    genre: "Romance",
-    release_year: 1997,
-    image: "http://localhost:3000/img/movies_cover/titanic.jpg",
-  },
-  {
-    id: 4,
-    title: "The Matrix",
-    director: "The Wachowskis",
-    genre: "Action",
-    release_year: 1999,
-    image: "http://localhost:3000/img/movies_cover/matrix.jpg",
-  },
-  {
-    id: 5,
-    title: "Interstellar",
-    director: "Christopher Nolan",
-    genre: "Science Fiction",
-    release_year: 2014,
-    image: "http://localhost:3000/img/movies_cover/interstellar.jpg",
-  },
-];
+const apiUrl = import.meta.env.VITE_API_URL + "/movies";
 
 export default function MoviesList() {
+  const [movies, setMovies] = useState([]);
+
+  const fetchMovies = () => {
+    axios.get(apiUrl).then((res) => {
+      setMovies(res.data.data);
+      console.log(res.data.data);
+    });
+  };
+
+  useEffect(fetchMovies, []);
+
   return (
     <div className="container my-5">
       <div className="row row-cols-3 g-3">
