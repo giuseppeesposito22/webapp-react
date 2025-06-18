99787b41185e0041380ec67c92fad9c945c1926f
giuseppeesposito22 <gius.espo22@gmail.com>
2025-06-18 13:33:09 +0200

    fecth movie in movie detail page

--- src/pages/MoviesDetailPage.jsx
+++ src/pages/MoviesDetailPage.jsx
@@ -1,72 +1,50 @@
 import { useParams } from "react-router-dom";
 import ReviewsList from "../components/ReviewsList";
-
-const movie = {
-  id: 1,
-  title: "Inception",
-  director: "Christopher Nolan",
-  genre: "Science Fiction",
-  release_year: 2010,
-  abstract:
-    "A skilled thief is given a chance at redemption if he can successfully perform inception.",
-  image: "http://localhost:3000/img/movies_cover/inception.jpg",
-  reviews: [
-    {
-      id: 1,
-      movie_id: 1,
-      name: "Alice",
-      vote: 5,
-      text: "A mind-bending masterpiece.",
-      created_at: "2024-11-29T10:40:13.000Z",
-      updated_at: "2024-11-29T10:40:13.000Z",
-    },
-    {
-      id: 2,
-      movie_id: 1,
-      name: "Bob",
-      vote: 4,
-      text: "Great visuals and a compelling story.",
-      created_at: "2024-11-29T10:40:13.000Z",
-      updated_at: "2024-11-29T10:40:13.000Z",
-    },
-    {
-      id: 3,
-      movie_id: 1,
-      name: "Charlie",
-      vote: 3,
-      text: "Confusing at times, but worth watching.",
-      created_at: "2024-11-29T10:40:13.000Z",
-      updated_at: "2024-11-29T10:40:13.000Z",
-    },
-  ],
-};
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 export default function MoviesDetailPage() {
   const { id } = useParams();
 
+  const apiUrl = import.meta.env.VITE_API_URL + "/movies/" + id;
+
+  const [movie, setMovie] = useState();
+
+  const fecthMovie = () => {
+    axios.get(apiUrl).then((res) => {
+      setMovie(res.data.data);
+      console.log(res.data.data);
+    });
+  };
+
+  useEffect(fecthMovie, []);
+
   return (
     <>
       <h1>Dettaglio film {id}</h1>
-
-      <div className="container">
-        <div className="row">
-          <div className="col-5">
-            <div className="card">
-              <img src={movie.image} alt="" />
+      {movie ? (
+        <div className="container">
+          <div className="row">
+            <div className="col-5">
+              <div className="card">
+                <img src={movie.image} alt="" />
+              </div>
+            </div>
+            <div className="col-7">
+              <p>{movie.title}</p>
+              <p>{movie.director}</p>
+              <p>{movie.genre}</p>
+              <p>{movie.release_year}</p>
             </div>
           </div>
-          <div className="col-7">
-            <p>{movie.title}</p>
-            <p>{movie.director}</p>
-            <p>{movie.genre}</p>
-            <p>{movie.release_year}</p>
-          </div>
+
+          <section className="my-5">{movie.abstract}</section>
+
+          <ReviewsList reviews={movie.reviews} />
         </div>
-
-        <section className="my-5">{movie.abstract}</section>
-
-        <ReviewsList reviews={movie.reviews} />
-      </div>
+      ) : (
+        <h1>Loading...</h1>
+      )}
     </>
   );
 }
