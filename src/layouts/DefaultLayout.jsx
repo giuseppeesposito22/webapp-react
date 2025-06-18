ceaab0f13dec87d60d40683c6681f93c86e105e5
giuseppeesposito22 <gius.espo22@gmail.com>
2025-06-18 16:30:08 +0200

    add loader in layout

--- src/layouts/DefaultLayout.jsx
+++ src/layouts/DefaultLayout.jsx
@@ -1,9 +1,14 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
+import Loader from "../components/Loader";
+import { useLoader } from "../contexts/LoaderContext";
 
 export default function DefaultLayout() {
+  const { isLoading } = useLoader();
+
   return (
     <>
+      <Loader show={isLoading} />
       <header>
         <Navbar />
       </header>
