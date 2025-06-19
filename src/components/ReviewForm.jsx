d4528a50fa4196d01cab4cf2f12ecb1cb58e1e60
giuseppeesposito22 <gius.espo22@gmail.com>
2025-06-19 16:24:12 +0200

    create ReviewForm component

---
+++ src/components/ReviewForm.jsx
@@ -0,0 +1,75 @@
+import { useState } from "react";
+
+const initialFormData = {
+  name: "",
+  vote: 0,
+  text: "",
+};
+
+export default function ReviewForm({ idMovie }) {
+  const [formData, setFormData] = useState(initialFormData);
+
+  const handleInputChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    setFormData(initialFormData);
+  };
+
+  return (
+    <>
+      <form className="row my-5" onSubmit={handleFormSubmit}>
+        <div className="col-6">
+          <label htmlFor="NameInput" className="form-label">
+            Name
+          </label>
+          <input
+            name="name"
+            value={formData.name}
+            onChange={handleInputChange}
+            className="form-control"
+            type="text"
+            id="NameInput"
+            required
+          ></input>
+        </div>
+        <div className="col-6">
+          <label htmlFor="voteInput" className="form-label">
+            Vote
+          </label>
+          <input
+            name="vote"
+            value={formData.vote}
+            onChange={handleInputChange}
+            className="form-control"
+            type="number"
+            id="voteInput"
+            min={1}
+            max={5}
+            required
+          ></input>
+        </div>
+        <div className="col-12">
+          <label htmlFor="textInput" className="form-label">
+            Text
+          </label>
+          <textarea
+            name="text"
+            value={formData.text}
+            onChange={handleInputChange}
+            className="form-control"
+            type="text"
+            id="textInput"
+            rows={5}
+            required
+          ></textarea>
+        </div>
+        <div className="col-4">
+          <button className="btn btn-primary my-2">Add review</button>
+        </div>
+      </form>
+    </>
+  );
+}
